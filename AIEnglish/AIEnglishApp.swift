fab616947ab9ee2c4b64599fef5efcba52a47324
Leonid117 <Leonid.v117@gmail.com>
2023-06-11 04:27:30 +0800

    Initial Commit

---
+++ AIEnglish/AIEnglishApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIEnglishApp.swift
+//  AIEnglish
+//
+//  Created by Leonid Volkov on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AIEnglishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
